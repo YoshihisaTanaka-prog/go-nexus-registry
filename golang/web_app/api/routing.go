ed0e2fc062801442231cd649ff6d348f129aadc0
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-10-12 13:45:25 +0900

    WebAPI機能の追加

---
+++ golang/web_app/api/routing.go
@@ -0,0 +1,42 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+var pagePathes = []string{
+	"sign-up",
+	"apply",
+}
+
+func Start() {
+	r := gin.Default()
+
+	// // TodoエンティティのCRUDエンドポイントを設定
+	// r.POST("/todos", func(c *gin.Context) { /* 作成処理 */ })
+	// r.GET("/todos", func(c *gin.Context) { /* 一覧取得処理 */ })
+	// r.GET("/todos/:id", func(c *gin.Context) { /* 個別取得処理 */ })
+	// r.PUT("/todos/:id", func(c *gin.Context) { /* 更新処理 */ })
+	// r.DELETE("/todos/:id", func(c *gin.Context) { /* 削除処理 */ })
+
+	// 必要に応じて静的ファイルのルートを設定
+	r.Static("/assets", "/app/public/assets")
+	for _, path := range pagePathes {
+		r.GET("/" + path, func(c *gin.Context) {
+			c.File("/app/public/htmls/" + path + ".html")
+		})
+	}
+
+	// /favicon.ico -> favicon.ico を返す
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.File("/app/public/favicon.ico")
+	})
+
+	// / → index.html を返す
+	r.GET("/", func(c *gin.Context) {
+		c.File("/app/public/htmls/index.html")
+	})
+
+	// サーバーを起動
+	r.Run("0.0.0.0:8080")
+}
