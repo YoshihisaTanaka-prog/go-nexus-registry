5611f5100ca085fe243cd440af1d0b81e31e2fc0
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-10-13 14:51:52 +0900

    LDAPの結果をHTTP Responseに反映

---
+++ golang/web_app/api/handler/sign.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"fmt"
+	"os"
+	"github.com/gin-gonic/gin"
+	"web_app/ldap"
+)
+
+func SignUp(c *gin.Context) {
+	// ctx = c.Request.Context()
+
+	var body struct {
+		Email  string `json:"email" binding:"required"`
+		Password  string `json:"password" binding:"required"`
+	}
+
+	if err := c.BindJSON(&body); err != nil {
+		fmt.Fprintln(os.Stderr, "sign-up\nリクエストJSONの解析に失敗しました\n", err)
+		c.JSON(400, gin.H{"error": "リクエストJSONの解析に失敗しました"})
+		return
+	}
+
+	fmt.Fprintln(os.Stdout, "sign-up:", body)
+
+	message, code := ldap.AddUser(body.Email, body.Password)
+	fmt.Println(message, code)
+
+	if code == 0 {
+		SetCookie(c, "test", "test")
+		c.JSON(200, gin.H{})
+	} else {
+		c.JSON(code, gin.H{"message": message})
+	}
+}
+
+func SignIn(c *gin.Context) {
+	// ctx = c.Request.Context()
+
+	var body struct {
+		Email  string `json:"email" binding:"required"`
+		Password  string `json:"password" binding:"required"`
+	}
+
+	if err := c.BindJSON(&body); err != nil {
+		fmt.Fprintln(os.Stderr, "sign-up\nリクエストJSONの解析に失敗しました\n", err)
+		c.JSON(400, gin.H{"error": "リクエストJSONの解析に失敗しました"})
+		return
+	}
+
+	fmt.Fprintln(os.Stdout, "sign-in:", body)
+
+	message, code := ldap.Authenticate(body.Email, body.Password)
+	fmt.Println(message, code)
+
+	if code == 0 {
+		SetCookie(c, "test", "test")
+		c.JSON(200, gin.H{})
+	} else {
+		c.JSON(code, gin.H{"message": message})
+	}
+}
