5f978a330c42fc18166e26e1c2fa8da35e93e91c
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-10-22 20:50:22 +0900

    一旦全部コメントアウト

--- golang/web_app/api/handler/sse.go
+++ golang/web_app/api/handler/sse.go
@@ -1,32 +1,33 @@
 package handler
 
 import (
-	"fmt"
-	"time"
+	// "fmt"
 	"github.com/gin-gonic/gin"
+	// "web_app/pubsub"
 )
 
 func SSE(c *gin.Context) {
-	userId := c.MustGet("userId").(string)
-	fmt.Println(userId)
-	// Content-Type を明示的に指定
-	c.Writer.Header().Set("Content-Type", "text/event-stream")
-	c.Writer.Header().Set("Cache-Control", "no-cache")
-	c.Writer.Header().Set("Connection", "keep-alive")
+	// // Content-Type を明示的に指定
+	// c.Writer.Header().Set("Content-Type", "text/event-stream")
+	// c.Writer.Header().Set("Cache-Control", "no-cache")
+	// c.Writer.Header().Set("Connection", "keep-alive")
 
-	ticker := time.NewTicker(2 * time.Second)
-	defer ticker.Stop()
+	// userId := c.MustGet("userId").(string)
+	// fmt.Println(userId)
 
-	for {
-		select {
-		case t := <-ticker.C:
-			// クライアントに送信
-			fmt.Fprintf(c.Writer, "data: 現在時刻は %s\n\n", t.Format("15:04:05"))
-			c.Writer.Flush() // バッファを明示的に送信
-		case <-c.Request.Context().Done():
-			// クライアント切断時
-			fmt.Println("クライアント切断")
-			return
-		}
-	}
+	// uuidSub, cancelSubscription := pubsub.SubscribeUuid(c.Request.Context(), userId)
+
+	// for {			
+	// 	select {
+	// 	case msg := <-uuidSub:
+	// 		// クライアントに送信
+	// 		fmt.Fprintf(c.Writer, "data: UUID: %s\n\n", msg)
+	// 		c.Writer.Flush() // バッファを明示的に送信
+	// 	case <-c.Request.Context().Done():
+	// 		// クライアント切断時
+	// 		fmt.Println("クライアント切断")
+	// 		cancelSubscription()
+	// 		return
+	// 	}
+	// }
 }
