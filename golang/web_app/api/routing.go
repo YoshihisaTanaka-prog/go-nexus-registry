79309af6935791e98481fd85655f8881a8acd9ba
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-10-22 21:00:06 +0900

    ・教材にあったエンドポイントを削除 ・依頼されたライブラリをステージング環境に格納するためのエンドポイントを追加 ・SSEのエンドポイントを削除

--- golang/web_app/api/routing.go
+++ golang/web_app/api/routing.go
@@ -13,10 +13,12 @@ var pagePaths = []string{
 	"sign-up",
 	"sign-in",
 	"apply",
+	"allow",
 }
 
 func Start() {
 	handler.InitJwt()
+	handler.SetNpmrc()
 
 	time.Sleep(time.Second * 1)
 	fmt.Fprintln(os.Stdout, "Webサーバを起動します。")
@@ -25,21 +27,15 @@ func Start() {
 
 	r.Use(handler.AuthProxy)
 
-	// // TodoエンティティのCRUDエンドポイントを設定
-	// r.POST("/todos", func(c *gin.Context) { /* 作成処理 */ })
-	// r.GET("/todos", func(c *gin.Context) { /* 一覧取得処理 */ })
-	// r.GET("/todos/:id", func(c *gin.Context) { /* 個別取得処理 */ })
-	// r.PUT("/todos/:id", func(c *gin.Context) { /* 更新処理 */ })
-	// r.DELETE("/todos/:id", func(c *gin.Context) { /* 削除処理 */ })
-
 	// /api グループを作成
 	api := r.Group("/api/v1")
 	{
 		api.POST("/sign-up", handler.SignUp)
 		api.POST("/sign-in", handler.SignIn)
+		api.POST("/apply", handler.Apply)
 	}
 
-	r.GET("/sse", handler.SSE)
+	// r.GET("/sse", handler.SSE)
 
 	// 必要に応じて静的ファイルのルートを設定
 	r.Static("/assets", "/app/public/assets")
