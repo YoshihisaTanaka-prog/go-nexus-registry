03d1a78f7285259ff7458b32344700ad02def565
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-10-28 17:55:32 +0900

    ビルドコマンドの変更

--- golang/build.sh
+++ golang/build.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 if [ "$#" = "1" ]; then
+  apt-get update && apt-get install -y gcc libsqlite3-dev
   cd "$(dirname $0)/${1}"
-  if [ ! -e go.mod ]; then
-    go mod init ${1}
-  fi
   go mod tidy
-  go build -o server main.go
+  CGO_ENABLED=1 go build -o server main.go
 else
   echo "Please select target directory"
   exit 1
