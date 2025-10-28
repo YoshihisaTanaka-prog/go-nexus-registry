d38f341b0ef9728570368b78d9d00e562cdc5562
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2025-10-28 17:52:45 +0900

    RAM DB用のDSNを変更

--- golang/web_app/dbClient/global.go
+++ golang/web_app/dbClient/global.go
@@ -8,8 +8,8 @@ import (
 
 var (
 	psqlDsn = "host=postgres port=5432 user=" + os.Getenv("POSTGRES_USER") + " dbname=addon_db password=" + os.Getenv("POSTGRES_PASSWORD") + " sslmode=disable"
-  psqlClient *ent.Client
-	ramDsn = "file:ent?mode=memory&cache=shared&_fk=1"
+  	psqlClient *ent.Client
+	ramDsn = "file::memory:?cache=shared&_fk=1"
 	ramClient *ent.Client
 	ctx *context.Context
 )
